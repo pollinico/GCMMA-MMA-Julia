d20148747ca50ed00bd1e1ef8543dbf4c18ca90a
Nicolo Pollini <24714270+pollinico@users.noreply.github.com>
2023-05-18 20:49:26 +0300

    fixed header

--- mmasub.jl
+++ mmasub.jl
@@ -1,5 +1,5 @@
 ########################################################################################################
-### GCMMA-MMA-Julia         															             ### 
+### GCMMA-MMA-Julia                                                                                  ### 
 ###                                                                                                  ###
 ### This file is part of GCMMA-MMA-Julia. GCMMA-MMA-Julia is licensed under the terms of GNU         ###
 ### General Public License as published by the Free Software Foundation. For more information and    ###
